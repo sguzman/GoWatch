e5810bf21e0654af5a1aace23bad29189b2d88bf
Salvador Guzman <guzmansalv@gmail.com>
2018-03-26 21:07:33 -0700

    massage data

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import {shows} from '../../data/shows'
+import {AnimeLinksInterface} from "../../interfaces/animelinks.interface";
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public shows: AnimeLinksInterface[] = [];
 
   constructor(public navCtrl: NavController) {
+    this.shows = Object.keys(shows).map(a => shows[a]);
 
+    console.log(this.shows);
   }
 
 }
